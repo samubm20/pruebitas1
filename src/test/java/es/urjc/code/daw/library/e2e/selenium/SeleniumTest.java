e1b9e2c323bc1315e77fe222ece26092fab721e9
danielestevezrecio <danielesstevezrecio@gmail.com>
2021-06-29 17:26:02 +0200

    Modificamos el test de Selenium

--- src/test/java/es/urjc/code/daw/library/e2e/selenium/SeleniumTest.java
+++ src/test/java/es/urjc/code/daw/library/e2e/selenium/SeleniumTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -36,7 +37,9 @@ public class SeleniumTest {
 
 	@BeforeEach
 	public void setupTest() {
-        this.driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        this.driver = new ChromeDriver(options);
         this.wait = new WebDriverWait(driver, 10);
 	}
 
